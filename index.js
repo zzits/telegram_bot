432ba4cb09c8abfc1b2119b5a81fe338f3fa3ba6
zzits <niklybomir@gmail.com>
2023-11-29 00:55:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bot = new TelegramApi(TOKEN, {polling: true})
 
 const admin = "1010086583"; 
 
+const userStates = {
+};
+
 bot.setMyCommands([
     { command: "/consultation", description: "Запис на консультацію"}
 ])
@@ -17,54 +20,105 @@ const benefits = {
         ]
     })
 }
+const contactKeyboard = {
+    reply_markup: JSON.stringify({
+        one_time_keyboard: true,
+        keyboard: [
 
-bot.onText(/\/consultation/, (msg) => {
+            [{
+                text: "Відпрвити данні акаунта",
+                request_contact: true
+            }],
+            [{
+                text: "Не відправляти"
+            }]
+        ]
+    })
+}
+
+bot.onText(/\/consultation/, async (msg) => {
     const chatId = msg.chat.id;
     const username = msg.from.username
-
-    bot.sendMessage(chatId, 'Введіть ваше ПІБ:').then(() => {
-        bot.once('text', (name) => {
-            const PIB = name.text;
-            bot.sendMessage(chatId, 'Наявність пільг', benefits)
-            .then((benefits_msg) => {
-                bot.once('callback_query', (response) => {
-                    const benefitsId = benefits_msg.message_id;
-                    bot.deleteMessage(chatId, benefitsId)
-                    if (response.data === 'false') {
-                        bot.sendMessage(chatId, 'Поверхово опишіть свій запит:')
-                        .then(() => {
-                            bot.once("text", (description) =>{
-                                bot.sendMessage(chatId, "Ваш запит було відправленно!")
-                                bot.sendMessage(admin, 
-                                    "Користувач: <b>@" + username +
-                                    "\n</b>ПІБ: <b>"+PIB+
-                                    "\n</b>Пільги відсутні"+
-                                    "\nПричина запиту: <b>" + description.text +"</b>"
-                                    , { parse_mode: 'HTML' })
-                            })
-                        })
-                    } else if (response.data === 'true') {
-                        bot.sendMessage(chatId, 'Відправте фото/документ пільг')
-                        .then(() => {
-                            bot.once("message", (benefitsData) =>{
-
+    console.log(userStates[String(chatId)], chatId)
+    if (!userStates[String(chatId)]) {
+        userStates[String(chatId)] = true
+        bot.sendMessage(chatId, 'Введіть ваше ПІБ:').then(() => {
+            bot.once('text', (name) => {
+                if (chatId!==name.chat.id) {
+    
+                }
+                else{
+                    const PIB = name.text;
+                    console.log("dfafjl;kdsa;jlkf")
+                    bot.sendMessage(chatId, 'Наявність пільг', benefits)
+                    .then((benefits_msg) => {
+                        bot.once('callback_query', (response) => {
+                            const benefitsId = benefits_msg.message_id;
+                            bot.deleteMessage(chatId, benefitsId)
+                            if (response.data === 'false') {
                                 bot.sendMessage(chatId, 'Поверхово опишіть свій запит:')
-                                .then(() =>{
+                                .then(() => {
                                     bot.once("text", (description) =>{
-                                        bot.sendMessage(chatId, "Ваш запит було відправленно!")
-                                        bot.sendMessage(admin, 
-                                            "Користувач: <b>@" + username +
-                                            "\n</b>ПІБ: <b>"+PIB+
-                                            "\n</b>Причина запиту: <b>" + description.text +"</b>"
-                                            , { parse_mode: 'HTML' })
-                                            bot.forwardMessage(admin, chatId, benefitsData.message_id)
+                                        bot.sendMessage(chatId, "Відправте данні акаунта для подальшо зв'язку", contactKeyboard)
+                                        .then(()=>{
+                                            bot.once("contact", (user) =>{
+                                                const phoneNumber = user.contact.phone_number;
+                                                const firstName = user.contact.first_name;
+                                                bot.sendMessage(chatId, "Ваш запит було відправленно!", { reply_markup:{ remove_keyboard: true }})
+                                                bot.sendMessage(admin,
+                                                    "ПІБ: <b>"+PIB+
+                                                    "\n</b>Пільги відсутні"+
+                                                    "\nПричина запиту: <b>" + description.text +"</b>"
+                                                    , { parse_mode: 'HTML' }
+                                                )
+                                                bot.sendContact(admin,phoneNumber,firstName)
+                                            })
+                                            bot.once("text", () =>{
+                                                bot.sendMessage(chatId, "Ваш запит було відправленно!", { reply_markup: { remove_keyboard: true } })
+                                                bot.sendMessage(admin,
+                                                    getUsername(username)+
+                                                    "ПІБ: <b>" + PIB +
+                                                    "\n</b>Пільги відсутні" +
+                                                    "\nПричина запиту: <b>" + description.text + "</b>"
+                                                    , { parse_mode: 'HTML' }
+                                                )
+                                            })
                                         })
                                     })
                                 })
-                            })
-                    }
-                });
+                            } 
+                            else if (response.data === 'true') {
+                                bot.sendMessage(chatId, 'Відправте фото/документ пільг')
+                                .then(() => {
+                                    bot.once("message", (benefitsData) =>{
+        
+                                        bot.sendMessage(chatId, 'Поверхово опишіть свій запит:')
+                                        .then(() =>{
+                                            bot.once("text", (description) =>{
+                                                bot.sendMessage(chatId, "Ваш запит було відправленно!")
+                                                bot.sendMessage(admin, 
+                                                    "Користувач: <b>@" + username +
+                                                    "\n</b>ПІБ: <b>"+PIB+
+                                                    "\n</b>Причина запиту: <b>" + description.text +"</b>"
+                                                    , { parse_mode: 'HTML' }
+                                                )
+                                                bot.forwardMessage(admin, chatId, benefitsData.message_id)
+                                            })
+                                        })
+                                    })
+                                })
+                            }
+                        });
+                    });
+                }
             });
         });
-    });
+    }
 });
+// Нужно создать обьект в котором будет пользователи и у пользователей будут параметры "step" и "action"
+    if (username === undefined) {
+        console.log(username)
+        return "Користувач не надав данні\n";
+    }
+    return "Користувач: <b>@" + username +"</b> \n";
+}
